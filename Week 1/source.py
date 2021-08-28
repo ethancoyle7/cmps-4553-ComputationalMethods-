00160fab26fe0bee6518335166a3f55319e11ee5
Anon-583 <77364000+ethancoyle7@users.noreply.github.com>
2021-08-28 09:57:14 -0500

    Create source.py

---
+++ Week 1/source.py
@@ -0,0 +1,12 @@
+##this first portion is creating  a 
+# very simple hello world program
+print("hello world") #prints hello world
+
+# this second portion is taking two #'s
+#and adding them both together and 
+#printing out the sum 
+
+x=5 # first input
+y=5 # second input
+output=x+y# sum  them both together
+print(output) #prints x+y

c3d271acd6696f59b49fc17cddd685eca153644d
Anon-583 <77364000+ethancoyle7@users.noreply.github.com>
2021-08-29 15:37:10 -0500

    Create arithmatic.py

---
+++ personalprojects/arithmatic.py
@@ -0,0 +1,20 @@
+First_Num = float(input("Enter First Number : "))
+Second_Num = float(input("Enter your second number: "))
+
+#below is the list of the assigned values
+#after performing arrithmetic procedures
+addition = First_Num + Second_Num
+subtraction = First_Num - Second_Num
+multiply= First_Num * Second_Num
+divide = First_Num / Second_Num
+modulo = First_Num % Second_Num
+exponent = First_Num ** Second_Num
+
+#print out various arithmatic
+
+print("The Sum of your Numbers Are ",addition)
+print("The Subtraction of the Two number is ",subtraction)
+print("By Multiplying, your result is : ", multiply)
+print("By Divding your numbers, the result is :",divide)
+print("The Modulo of your numbers is : ", modulo)
+print("Taking the exponent of your numbers, we get : ",exponent)

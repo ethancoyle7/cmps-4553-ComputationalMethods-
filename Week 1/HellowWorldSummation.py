4aec054714234e033ae655253368ffd95f51ac7b
Anon-583 <77364000+ethancoyle7@users.noreply.github.com>
2021-08-29 14:05:36 -0500

    Update HellowWorldSummation.py

--- Week 1/HellowWorldSummation.py
+++ Week 1/HellowWorldSummation.py
@@ -1,17 +1,30 @@
-#Ethan Coyle
-#CMPS4553-Computational Methods
-#Dr.StringFellow
-#8/28/2021 Homework 1 and 2
+######################################################
+## Author:       Ethan Coyle                        ##
+## Instructor:    Dr StringFellow                   ##
+## Class     :    CMPS 4553-Computational Methods   ##
+## Class Time:    Tuesday,Thursday 9:30AM           ##
+## Assignment:    Homework 1 and 2                  ##
+## Due Date  :    Thursday September 2, 2021        ##
+######################################################
 
-##this first portion is creating  a 
-# very simple hello world program
-print("hello world") #prints hello world
+# The purpose of this program is to create a simple 
+# Hello World Program as well as a summation of 
+# two numbers
 
-# this second portion is taking two #'s
-#and adding them both together and 
-#printing out the sum 
+##Part 1 : Hello World
+print("Hello World")
 
-x=5 # first input
-y=5 # second input
-output=x+y# sum  them both together
-print(output) #prints x+y
+
+#part 2: summation of 2 numbers
+
+#user input 2 numbers
+Num1 = input(" Please Enter the First Number: ")
+Num2 = input(" Please Enter the second number: ")
+
+# Using arithmetic + Operator to add two numbers
+#read in as a string and converted to int or else
+#will concatinata as a string input
+sum = int(Num1) + int(Num2)
+
+#print the result
+print("The summation of ",Num1," and ",Num2," is: ",sum )

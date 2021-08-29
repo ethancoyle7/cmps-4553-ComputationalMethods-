f106d488076c24eba69898a513dbd25db3c6fc3f
Anon-583 <77364000+ethancoyle7@users.noreply.github.com>
2021-08-29 15:42:22 -0500

    Create CalanderDate.py

---
+++ personalprojects/CalanderDate.py
@@ -0,0 +1,13 @@
+# import calendar module
+import calendar
+
+# this excersise demonstarts the 
+#python calander to display the calander month
+#from python by entering the month and year
+print("WE WILL DISPLAY THE MONTH OUTPUT FOR YOUR INPUT\n")
+year = int(input("Enter the Year: "))
+month = int(input("Enter the Month: "))
+
+# Displaying calander from the python inport
+#for the user input
+print("\n",calendar.month(year, month))

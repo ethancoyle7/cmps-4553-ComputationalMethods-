e2e12ee84f4d119e590be53278798cb2defe8276
Anon-583 <77364000+ethancoyle7@users.noreply.github.com>
2021-08-29 13:42:12 -0500

    Create Indexing.py

---
+++ personalprojects/Indexing.py
@@ -0,0 +1,6 @@
+List = [10, 20, 30, 40, 50,60,70,80,90,100]
+
+print("\n This Output Shows the Index Value as well as the Contents Inside \n")
+#for loop to loop through the array list and print out value and its index num
+for i in range(len(List)):
+    print("INDEX # = ", i, " Index Value = ", List[i])

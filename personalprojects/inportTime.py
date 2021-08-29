794100bc31b897b3ee7c818f70986f49471c8bd5
Anon-583 <77364000+ethancoyle7@users.noreply.github.com>
2021-08-29 15:46:28 -0500

    Create inportTime.py

---
+++ personalprojects/inportTime.py
@@ -0,0 +1,9 @@
+import datetime
+ 
+#this excersice just shows pythons usage of current
+#date and time
+
+#get the date and time from inport then print out
+
+CurrentDate_Time = datetime.datetime.now()
+print("Today's Date and Time = ", CurrentDate_Time)
